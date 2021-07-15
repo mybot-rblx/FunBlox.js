1200ed1a6377d869dd3f3dfb61a4d30c9d5a09cf
Fede1212156 <72459034+Fede1212156@users.noreply.github.com>
2021-07-15 17:45:23 -0300

    getUserRank.js
    Get group rank. (group id)

---
+++ src/getUserRank.js
@@ -0,0 +1,40 @@
+
+
+const fetch = require('node-fetch');
+
+async function getGroupRank(Data) {
+    return new Promise((resolve, reject) => {
+        let body = await fetch(`https://groups.roblox.com/v2/users/${Data.userId}/groups/roles`, {
+            headers: {"Content-Type": "application/json"}
+        })
+
+        body = await body.json();
+        const groupObject = body.data.find((info) => Data.groupId === info.group.id)
+
+        return groupObject ? parseInt(groupObject.role.rank) : 0
+    })
+}
+
+module.exports = async function(group, user) {
+    return new Promise((resolve, reject) => {
+      if(!group){
+      throw new TypeError("Please enter a valid GROUP_ID. The format is: roblox.getUserRank(groupid, userid)");
+      }
+      if(!user){
+        throw new TypeError("Please enter a valid USER_ID. The format is: roblox.getUserRank(groupid, userid)");
+      }
+      let body = await fetch(`https://groups.roblox.com/v2/users/${Data.userId}/groups/roles`, {
+        headers: {"Content-Type": "application/json"}
+        }).then(async (response) => {
+        if(response.success !== 'undefined') {
+          if(response.status == 404) return reject("Not found.");
+        }
+        response = await response.json();
+        if(typeof response.errors !== 'undefined') {
+          if(errors[0].code == 1) return reject("Not found.");
+        }
+        getGroupRank(response).then(finished => {
+          resolve(finished);
+        });
+    })
+}
