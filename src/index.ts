531989a65330fd543993c68e2f93d2d76a301544
joshuadl12 <47647612+joshuadl12@users.noreply.github.com>
2021-10-09 21:58:40 -0500

    wow

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import getUserRank from './user/getUserRank';
 import getUserFavoriteGames from './user/getUserFavoriteGames';
 import getCategories from './catalog/getCategories';
 import getFavoritesOfBundle from './catalog/getFavoritesOfBundle';
+import getFavoritesOfItem from './catalog/getFavoritesOfItem';
 import getPlayerThumbnail from './user/getPlayerThumbnail';
 import * as api from './api'
 
@@ -15,5 +16,6 @@ export {
     getUserFavoriteGames,
     getFavoritesOfBundle,
     getCategories,
-    getPlayerThumbnail
+    getPlayerThumbnail,
+    getFavoritesOfItem
 }
