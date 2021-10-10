b39ccfd90dc8ee23b6dc90abb2c5aab555103a55
joshuadl12 <47647612+joshuadl12@users.noreply.github.com>
2021-10-10 10:29:52 -0500

    change timeout for api

--- src/api.ts
+++ src/api.ts
@@ -4,34 +4,34 @@ axios.defaults.headers.get["Content-Type"] = "application/json";
 
 export const api = axios.create({
     baseURL: 'https://api.roblox.com/',
-    timeout: 1000,
+    timeout: 2500,
 });
 
 export const groups = axios.create({
     baseURL: 'https://groups.roblox.com/',
-    timeout: 1000,
+    timeout: 2500,
 });
 
 export const games = axios.create({
     baseURL: 'https://games.roblox.com/',
-    timeout: 1000,
+    timeout: 2500,
 });
 
 export const catalog = axios.create({
     baseURL: 'https://catalog.roblox.com/',
-    timeout: 1000,
+    timeout: 2500,
 })
 
 export const friends = axios.create({
     baseURL: 'https://friends.roblox.com/',
-    timeout: 1000,
+    timeout: 2500,
 });
 export const thumbnails = axios.create({
     baseURL: 'https://thumbnails.roblox.com/',
-    timeout: 1000,
+    timeout: 2500,
 });
 
 export const users = axios.create({
     baseURL: 'https://users.roblox.com/',
-    timeout: 1000,
+    timeout: 2500,
 });
