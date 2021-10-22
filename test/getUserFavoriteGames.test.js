64614c62d1b9aa859a56addf31fbd2ba1e3378da
joshuadl12 <47647612+joshuadl12@users.noreply.github.com>
2021-10-22 17:34:37 -0500

    fixxer upper

--- test/getUserFavoriteGames.test.js
+++ test/getUserFavoriteGames.test.js
@@ -3,7 +3,7 @@ const wow = require("../dist");
 
 describe("FunBlox", () => {
     describe("#getUserFavpr", () => {
-        it("should return the user data via id", function () {
+        it("should return the user data via id", async function () {
             const data = await wow.getUserFavoriteGames(156711358)
 
             expect(data).toBeInstanceOf(Array);
