4bd20b4704c587a068bd297874784227777b65a1
joshuadl12 <47647612+joshuadl12@users.noreply.github.com>
2021-11-07 20:32:13 -0600

    fix parcel + other things

--- packages/funblox/src/catalog/getCategories.ts
+++ packages/funblox/src/catalog/getCategories.ts
@@ -1,14 +1,15 @@
+/* eslint-disable require-jsdoc */
 import * as Promise from 'bluebird';
-import { catalog } from '../api';
+import {catalog} from '../api';
 
 export default function getCategories(): Promise<object> {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const categories = await catalog.get('v1/categories')
-            
-            resolve(categories.data);
-        } catch (e) {
-            reject(e);
-        }
-    });
+  return new Promise(async (resolve, reject) => {
+    try {
+      const categories = await catalog.get('v1/categories');
+
+      resolve(categories.data);
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
