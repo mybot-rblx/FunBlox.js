4ffdf057a993507a97d0bda23f41a27e0dca7739
joshuadl12 <joshuadl12@icloud.com>
2022-02-24 21:22:19 -0600

    Switch back to roproxy

--- packages/funblox/src/api.ts
+++ packages/funblox/src/api.ts
@@ -8,42 +8,42 @@ export const thumbnailsCache = new Map();
 export const usersCache = new Map();
 
 export const api = got.extend({
-  prefixUrl: 'https://rprxy.mybot-rblx.tk/api/',
+  prefixUrl: 'https://api.roproxy.com/',
   responseType: 'json',
   cache: apiCache,
 });
 
 export const groups = got.extend({
-  prefixUrl: 'https://rprxy.mybot-rblx.tk/groups/',
+  prefixUrl: 'https://groups.roproxy.com/',
   responseType: 'json',
   cache: groupsCache,
 });
 
 export const games = got.extend({
-  prefixUrl: 'https://rprxy.mybot-rblx.tk/games/',
+  prefixUrl: 'https://games.roproxy.com/',
   responseType: 'json',
   cache: gamesCache,
 });
 
 export const catalog = got.extend({
-  prefixUrl: 'https://rprxy.mybot-rblx.tk/catalog/',
+  prefixUrl: 'https://catalog.roproxy.com/',
   responseType: 'json',
   cache: catalogCache,
 });
 
 export const friends = got.extend({
-  prefixUrl: 'https://rprxy.mybot-rblx.tk/friends/',
+  prefixUrl: 'https://friends.roproxy.com/',
   responseType: 'json',
   cache: catalogCache,
 });
 export const thumbnails = got.extend({
-  prefixUrl: 'https://rprxy.mybot-rblx.tk/thumbnails/',
+  prefixUrl: 'https://thumbnails.roproxy.com/',
   responseType: 'json',
   cache: thumbnailsCache,
 });
 
 export const users = got.extend({
-  prefixUrl: 'https://rprxy.mybot-rblx.tk/users/',
+  prefixUrl: 'https://users.roproxy.comß/',
   responseType: 'json',
   cache: usersCache,
 });
