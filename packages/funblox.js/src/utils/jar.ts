e4d0e067edeafebad0ef84280f1340d2d5a33161
joshuadl12 <joshuadl12@mybot-rblx.tk>
2022-05-18 21:56:55 -0500

    symlinking

--- packages/funblox.js/src/utils/jar.ts
+++
@@ -1,5 +0,0 @@
-import tough from 'tough-cookie';
-import { FileCookieStore } from 'tough-cookie-file-store';
-const jar = new tough.CookieJar(new FileCookieStore('./.cookies'));
-
-export default jar;
