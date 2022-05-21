3416e9d158c6b7f20558fe09aca81dd97cbcdb4d
joshuadl12 <joshuadl12@mybot-rblx.tk>
2022-05-20 19:02:54 -0500

    ESLint

--- packages/funblox/tests/getFavoritesOfBundles.test.ts
+++ packages/funblox/tests/getFavoritesOfBundles.test.ts
@@ -2,7 +2,7 @@ import { getFavoritesOfBundle } from '../';
 
 describe('FunBlox', () => {
   describe('#getUserBundles', () => {
-    it('should return number of favorites', function () {
+    it('should return number of favorites', function() {
       return getFavoritesOfBundle(589);
     });
   });
