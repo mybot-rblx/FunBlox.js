5172eeb01c695f926833cc43c29efa78fb995939
joshuadl12 <joshuadl12@mybot-rblx.tk>
2022-05-24 21:51:36 -0500

    Update yarn.lock and getRoles

--- packages/funblox/src/group/getRoles.ts
+++ packages/funblox/src/group/getRoles.ts
@@ -18,33 +18,30 @@ interface RoleJSON {
  * @param { number } groupid
  * @return {Promise<Roles>}
  */
- export default function getRoles( groupid: number ): Promise<Roles> {
-return new Promise(async (resolve, reject) => {
-  if (typeof groupid === 'number') {
-    const roleResponse: Response<string> = await groups.get(
-        `v1/groups/${groupid}/roles`,
-    );
-    const roles = JSON.parse(JSON.stringify(roleResponse.body));
-    const data = {
-      roles: []
-    }
+export default function getRoles( groupid: number ): Promise<Roles> {
+  return new Promise(async (resolve, reject) => {
+    if (typeof groupid === 'number') {
+      const roleResponse: Response<string> = await groups.get(
+          `v1/groups/${groupid}/roles`,
+      );
+      const roles = JSON.parse(JSON.stringify(roleResponse.body));
+      const data = {
+        roles: [],
+      };
 
-    for (const role of roles.roles) {
-      const i = roles.roles.indexOf(role);
-      data[0].roles.push({
-        id: role.id,
-        name: role.name,
-        membercount: role.memberCount,
-      });
-      if (i + 1 == roles.roles.length) return resolve(data);
-    }
+      for (const role of roles.roles) {
+        const i = roles.roles.indexOf(role);
+        data[0].roles.push({
+          id: role.id,
+          name: role.name,
+          membercount: role.memberCount,
+        });
+        if (i + 1 == roles.roles.length) return resolve(data);
+      }
 
-    return resolve(data);
-
-
-    }else{
+      return resolve(data);
+    } else {
       reject(new TypeError('Group param must be a number'));
     }
-
   });
 }
