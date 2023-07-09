0185041959c06718519e4f9f2d1d0811a8ab831a
joshuadl12 <hi@dogeflexjosh.tk>
2023-07-09 03:15:11 +0000

    yeah

--- packages/funblox/src/group/getGroupGames.ts
+++ packages/funblox/src/group/getGroupGames.ts
@@ -13,8 +13,8 @@ type GroupGame = {
 export default function getGroupGames(groupId: string | number): Promise<GroupGame[]> {
   return new Promise(async (resolve, reject) => {
     const finishedData = [];
-    const gamesInGroupData = games.get(`v2/groups/${groupId}/games`)
-    const gamesInGroup: GroupGamesAPIResponse = JSON.parse(JSON.stringify(gamesInGroupData))
+    const gamesInGroupData = await games.get(`v2/groups/${groupId}/games`)
+    const gamesInGroup: GroupGamesAPIResponse = JSON.parse(JSON.stringify(gamesInGroupData.body));
     gamesInGroup.data.forEach(game => {
       finishedData.push({ id: game.id, name: game.name, description: game.description, placeVisits: game.placeVisits })
     });
